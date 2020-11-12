0e5c753187c6ef4191fc2f7f173feb6fadce3f91
Gustavo Tadei <gustavo.tadei@hotmail.com>
2020-11-12 17:27:07 -0400

    Exercicio 02 - Concluido

---
+++ 02/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TAM 2
+
+typedef struct aluno {
+    char nome[30];
+    char endereco[100];
+    char curso [30];
+} Aluno;
+
+int main() {
+    FILE *arquivo;
+    Aluno alunos[TAM];
+    int i;
+
+    arquivo = fopen("entrada.txt", "r");
+    if(arquivo == NULL) {
+        printf("Erro na abertura do arquivo.\n");
+        exit(1);
+    }
+
+    for(i = 0; i < TAM; i++) {
+        fscanf(arquivo, "%s %s %s", alunos[i].nome, alunos[i].endereco, alunos[i].curso);
+    }
+
+    fclose(arquivo);
+
+    //////////////////////////////////////////////////////////////////////////////////////
+
+    arquivo = fopen("saida_1.txt", "w");
+    if(arquivo == NULL) {
+        printf("Erro na abertura do arquivo.\n");
+        exit(1);
+    }
+
+    for(i = 0; i < TAM; i++) {
+        fprintf(arquivo, "%d%s", strlen(alunos[i].nome), alunos[i].nome);
+        fprintf(arquivo, "%d%s", strlen(alunos[i].endereco), alunos[i].endereco);
+        fprintf(arquivo, "%d%s", strlen(alunos[i].curso), alunos[i].curso);
+    }
+
+    fclose(arquivo);
+
+    return 0;
+}
